1902c181a55da52f617823825eb92f82366ed4cb
donmunna435-dev <donmunna435@gmail.com>
2026-01-25 08:14:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "24754824"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "e24a9c7a6aa24e1c56fa349e104ec20e")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "96824"))
+ADMINS = int(os.environ.get("ADMINS", "968292174"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
@@ -19,7 +19,7 @@ DB_NAME = os.environ.get("DB_NAME", "idfinderpro")
 
 # Force Subscription Channel
 FORCE_SUB_CHANNEL = "idfinderpro"  # Channel username without @
-FORCE_SUB_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+FORCE_SUB_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0"))
 
 # Log Channel - All downloaded files will be forwarded here
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "0"))  # Set to 0 to disable logging
